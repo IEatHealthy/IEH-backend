faa4f507e3ae1262b8c6e2a133e84d2a38c2c54f
Evan Thomas <thomae3@unlv.nevada.edu>
2018-04-30 22:45:27 -0700

    Revert back to GET request.

--- backend/src/main/java/info/ieathealthy/restcontrollers/RecipeController.java
+++ backend/src/main/java/info/ieathealthy/restcontrollers/RecipeController.java
@@ -790,10 +790,10 @@ public class RecipeController {
     //Used to display 20 recipes that the user can see. They are like featured/recommended recipes. The recipes displayed
     //will depend on the user's cooking skill and the top rated recipes. Because there are no rated recipes right now,
     //I won't be implementing it. It'll just get the first 20 recipes found based on the user's cooking skill.
-    @RequestMapping(value="/api/recipe/recommended/{email}", method=RequestMethod.POST)
-    public ResponseEntity<?> getRecommendedRecipes(@PathVariable(value="email") String email, @RequestBody Token tokeno){
+    @RequestMapping(value="/api/recipe/recommended/{email}", method=RequestMethod.GET)
+    public ResponseEntity<?> getRecommendedRecipes(@PathVariable(value="email") String email, @RequestParam(value = "token") String token){
         try {
-            String token = tokeno.getToken();
+
             Jwts.parser().setSigningKey(_sigKey).parseClaimsJws(token);
 
             User user = _userCollection.find(eq("email", email)).first();
