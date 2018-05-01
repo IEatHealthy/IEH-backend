f67813cc7b7dcdfcba6d1e09c1236e9a49231371
Evan Thomas <thomae3@unlv.nevada.edu>
2018-04-30 17:02:46 -0700

    Refactor description in Badge model.

--- backend/src/main/java/info/ieathealthy/models/Badge.java
+++ backend/src/main/java/info/ieathealthy/models/Badge.java
@@ -4,7 +4,7 @@ import org.bson.types.*;
 public class Badge {
     private ObjectId _id;
     private int badgeId;
-    private String decsription;
+    private String description;
     private boolean awardOnSignup;
 
     public Badge(){
@@ -27,12 +27,12 @@ public class Badge {
         this.badgeId = badgeId;
     }
 
-    public String getDecsription() {
-        return decsription;
+    public String getDescription() {
+        return description;
     }
 
-    public void setDecsription(String decsription) {
-        this.decsription = decsription;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public boolean isAwardOnSignup() {
